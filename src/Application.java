3d968253641cbaf0ede5d92e5e4568210dc1b84b
Cat <avelinaanisimova@gmail.com>
2023-04-11 12:48:11 +0300

    Task 1

--- src/Application.java
+++ src/Application.java
@@ -1,27 +1,35 @@
 import java.util.Scanner;
+
 /*
-* Кр - Король
-* Ф - Ферзь
-* Л - Ладья
-* С - Слон
-* К - Конь
-* для пешки в начале буквы пожалели
+ * B - Король
+ * F - Ферзь
+ * L - Ладья
+ * S - Слон
+ * K - Конь
+ * для пешки в начале буквы пожалели
  */
+
 public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        CommandValidator CV = new CommandValidator();
         int count = 5;
         int current = 1;
+        String hod;
         while (current <= count) {
-            if (current % 2 == 1){
-                String hod = sc.nextLine();
-                System.out.println("P1:" + hod);
+            hod = sc.nextLine();
+            if (CV.validate(hod)) {
+                if (current % 2 == 1) {
+                    System.out.println("P1:" + hod);
+                } else {
+                    System.out.println("P2: " + hod);
+                }
+                current++;
             }
-            else {
-                String hod = sc.nextLine();
-                System.out.println("P2: " + hod);
+            else{
+                System.out.println("Неверно введен ход");
             }
-            current++;
         }
     }
 }
+
