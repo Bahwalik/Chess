9175dea193ef7204611638a7ee0d52ec20c400c6
Cat <avelinaanisimova@gmail.com>
2023-04-18 12:51:11 +0300

    Create CellType and Cell.

---
+++ src/Cell.java
@@ -0,0 +1,14 @@
+public class Cell {
+
+
+    private CellType cellType;
+    private Position position;
+
+    public CellType getCellType() {
+        return cellType;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+}
